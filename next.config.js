998919375c4731bbdd31c11cab1f606d218ee05d
gretrix <admin@gretrix.com>
2025-11-24 21:41:57 +0530

    Add redirect from /meeting-jt to Calendly

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/meeting-jt',
+        destination: 'https://calendly.com/meeting-jt',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
